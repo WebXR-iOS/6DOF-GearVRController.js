db212be882591a2771ce86c10c2f411b9eb07265
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-04-22 18:19:26 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+/**
+ * @author crazyh / https://github.com/crazyh2
+ */
 
+import { ClientTracking } from "./clientTracking.js";
+import { ServerTracking } from "./serverTracking.js";
+
+export { ClientTracking, ServerTracking };
